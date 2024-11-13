2c2d811b2be701d9dfcd7841cb5cd19460d02396
BR <bairun@ist-12016.apn.wlan.private.upenn.edu>
2024-11-13 17:17:10 -0500

    vue v0.000000001

---
+++ components/DemoApp.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Vue 3 Demo App</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello Vue 3!'
+        }
+    }
+}
+</script>
